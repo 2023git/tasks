e2bd11e5194f6dd3954847fe7093070b93a66042
Lim Mikhail <129537885+2023git@users.noreply.github.com>
2023-06-26 16:01:53 +0600

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,6 +1,31 @@
-function getChar(param) {       
-    return String.fromCharCode(param)             
-}      
- 
+// 1
+function getChar(param) {
+    return String.fromCharCode(param)
+}
+
+
+// 3
+function greet(name) {
+  if (name === "Johnny") {
+    return "Hello, my love!";
+  }
+
+  return "Hello, " + name + "!";
+}
+
+
+// 3
+String.prototype.toAlternatingCase = function () {
+  let result = "";
+  for (let i = 0; i < this.length; i++) {
+    const char = this[i];
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+  return result;
+};
   
  
