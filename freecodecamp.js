379df73eeca13368a21b8b49c9cfefc3ee419bb0
Lim Mikhail <129537885+2023git@users.noreply.github.com>
2023-06-26 16:51:55 +0600

    Update freecodecamp.js

--- freecodecamp.js
+++ freecodecamp.js
@@ -1,4 +1,21 @@
-// This is an in-line comment.
-
+// 1
 /* This is a
 multi-line comment */ 
+
+
+// 2
+var myName;
+
+
+// 3
+var a;
+a = 7
+
+
+// 4
+var a
+var b = a
+
+//5
+var myVar = 0;
+var a = 9
