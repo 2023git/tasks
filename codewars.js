f746055d6ca0bfd5d99089492a80a1f9214a3cab
Lim Mikhail <129537885+2023git@users.noreply.github.com>
2023-10-13 06:09:01 +0600

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -2,12 +2,12 @@
 function getChar(param) {
     return String.fromCharCode(param) 
 }   
-    
+     
      
 // 3             
 function greet(name) {                 
   if (name === "Johnny") {           
-    return "Hello, my love!";            
+    return "Hello, my love!";             
   }         
     
   return "Hello, " + name + "!";   
